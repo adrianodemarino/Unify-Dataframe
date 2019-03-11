79ed526475bdeaa12d3b954176ddd92f4f8279e8
Adriano De Marino <adriano.demarino@gmail.com>
2019-03-11 23:22:13 +0100

    Create merge_dataframe.py
    Initial commit of the algorithm.

---
+++ merge_dataframe.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+
+def unify_new(list):
+
+    super = {}
+
+    for df in list:
+        value = df.values
+        for n,i in enumerate(df.index):
+            for k,j in enumerate(df.columns):
+                if j in super:
+                    if i in super[j]:
+                        super[j][i] += value[n,k]
+                    else:
+                        super[j][i] = value[n,k]
+                else:
+                    super[j] = {i : value[n,k]}
+
+    return pd.DataFrame.from_dict(super)
+
+list = [ df_1,df_2 ] 
+
+new_df = unify_new(list)
